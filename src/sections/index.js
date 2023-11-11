ce5e6443284b992c66684743d495f6885759f026
saquir ali <shakirali9581@gmail.com>
2023-11-11 18:06:38 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+  Footer,
+};
