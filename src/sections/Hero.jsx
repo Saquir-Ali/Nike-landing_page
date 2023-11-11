cf7cd07c8f33628bbfc127108eedcf0bb19e1746
saquir ali <shakirali9581@gmail.com>
2023-11-11 18:47:45 +0530

    change2

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen
-      gap-10 max-container"
+      gap-10 max-container p-2"
     >
       <div
         className="relative xl:w-2/5 flex-col
